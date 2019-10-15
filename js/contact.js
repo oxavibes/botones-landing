827d1f94d46f4c58d71e3c24f12cb0f067866f21
Stefano <bozzistef@gmail.com>
2019-10-15 14:39:24 -0400

    Adding recaptcha validation and the email template

--- js/contact.js
+++ js/contact.js
@@ -6,81 +6,101 @@
             return this.optional(element) || /^\bcat\b$/.test(value)
         }, "type the correct answer -_-");
 
-        function resetFields(){
-
-        }
-
+        jQuery.validator.addMethod("phone", function (phone_number, element) {
+            phone_number = phone_number.replace(/\s+/g, "");
+            return this.optional(element) || phone_number.length > 9 &&
+                phone_number.match(/^(?:(?:00|\+)58|0)(?:2(?:12|4[0-9]|5[1-9]|6[0-9]|7[0-8]|8[1-35-8]|9[1-5]|3[45789])|4(?:1[246]|2[46]))\d{7}$/);
+        }, "Por favor, ingresa un numero valido");
 
         // validate contactForm form
         $(function () {
             $('#contactForm').validate({
+                ignore: ".ignore",
                 rules: {
                     name: {
                         required: true,
-                        minlength: 4
                     },
                     email: {
+                        email: true,
                         required: true,
-                        email: true
+                    },
+                    tel: {
+                        required: true,
+                        phone: true
+                    },
+                    company: {
+                        required: true,
                     },
                     subject: {
                         required: true,
-                        minlength: 6
                     },
                     message: {
                         required: true,
                         minlength: 10
+                    },
+                    hiddenRecaptcha: {
+                        required: function () {
+                            if (grecaptcha.getResponse() == '') {
+                                return true;
+                            } else {
+                                return false;
+                            }
+                        }
                     }
                 },
                 messages: {
                     name: {
-                        required: "Por favor, ingresa tu nombre.",
+                        required: "Por favor, ingresa tu nombre",
                     },
                     email: {
-                        required: "Por favor, ingresa tu email."
+                        email: "Por favor, ingresa un email válido",
+                        required: "Por favor, ingresa tu email"
+                    },
+                    tel: {
+                        required: "Por favor, ingresa tu numero de telefono",
+                    },
+                    company: {
+                        required: "Por favor, ingresa el nombre de tu compañia"
                     },
                     subject: {
-                        required: "Por favor, ingresa el asunto"
+                        required: "Por favor, ingresa el asunto",
                     },
                     message: {
+                        minlength: "Por favor, el mensaje debe ser mayor a 10 caracteres",
                         required: "Por favor, ingresa tu mensaje",
+                    }, 
+                    hiddenRecaptcha: {
+                        required: "Por favor, complete el recaptcha"
                     }
                 },
                 submitHandler: function (form) {
-                    /*const zohoURL = "https://mail.zoho.com/api/accounts/2480525000000008002/messages";
-                    
-                    var headers = {
-                        'Authorization':'464156509920d89e15ed1736908aXXXX',
-                        'Content-Type':'application/json'
+
+                    if (grecaptcha.getResponse()) {
+                        $(form).ajaxSubmit({
+                            type: "POST",
+                            url: "contact_process.php",
+                            data: $(form).serialize(),
+
+                            success: function () {
+                                $('#contactForm :input').attr('disabled', 'disabled');
+                                $('#contactForm').fadeTo("slow", 1, function () {
+                                    //$(this).find(':input').attr('disabled', 'disabled');
+                                    $(this).resetForm();
+                                    $(this).find('label').css('cursor', 'default');
+                                    $('#success').fadeIn()
+                                })
+                            },
+                            error: function () {
+                                $('#contactForm').fadeTo("slow", 1, function () {
+                                    $('#error').fadeIn()
+                                })
+                            }
+                        })
+                    } else {
+                        alert('Please confirm captcha to proceed')
                     }
 
-                    const messageObject = {
-                        fromAddress: "admin@tubotones.com",
-                        toAddress: "admin@tubotones.com",
-                        subject: "Has recibido un correo desde la pagina de Botones",
-                        content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod dicta nulla facilis et natus corporis porro facere placeat magnam atque dolorum obcaecati distinctio commodi inventore totam, dolor necessitatibus nemo praesentium? "
-                
-                    };*/
 
-                    $(form).ajaxSubmit({
-                        type: "POST",
-                        url: "contact_process.php",
-                        data: $(form).serialize(),
-
-                        success: function () {
-                            $('#contactForm :input').attr('disabled', 'disabled');
-                            $('#contactForm').fadeTo("slow", 1, function () {
-                                //$(this).find(':input').attr('disabled', 'disabled');
-                                $(this).find('label').css('cursor', 'default');
-                                $('#success').fadeIn()
-                            })
-                        },
-                        error: function () {
-                            $('#contactForm').fadeTo("slow", 1, function () {
-                                $('#error').fadeIn()
-                            })
-                        }
-                    })
                 }
             })
         })
